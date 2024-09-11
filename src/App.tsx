092f694b890bc183c99007789306aacc7993459f
TAPIOKA1113 <manato20021113@gmail.com>
2024-09-11 17:32:50 +0900

    apiのパス変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import './App.css'
 import Field from './components/Field'
 import Button from './components/Button'
+// import Spotify from 'react-spotify-embed'
+import Iframe from "react-iframe";
 
 interface Song {
   index: number;
@@ -21,10 +23,11 @@ interface Setlist {
   venue: string;
   tour_name: string;
   songs: Song[];
+  setlist_id: string;
 }
 
 export async function getSetlistFromSetlistFm(setlistFmId: string): Promise<Setlist> {
-  const url = `http://localhost:3000/api/setlist/${setlistFmId}`;
+  const url = `http://localhost:3000/api/setlistfm/${setlistFmId}`;
   const headers = {
     "x-api-key": "rvH9s-nOQE4FOGgLByWj1VfmjzqIaEt5Q8wB",
     "Accept": "application/json",
@@ -35,16 +38,11 @@ export async function getSetlistFromSetlistFm(setlistFmId: string): Promise<Setl
   return response.data;
 };
 
-export async function getSetlistFromLiveFans(): Promise<Setlist> {
+export async function getSetlistFromLiveFans(liveFansID: string): Promise<Setlist> {
 
-  const url = `http://localhost:3000/scrape`;
+  const url = `http://localhost:3000/api/livefans/${liveFansID}`;
 
-  const headers = {
-    "Accept": "application/json",
-    'Access-Control-Allow-Origin': '*'
-  };
-
-  const response = await axios.get(url, { headers });
+  const response = await axios.get(url);
   return response.data;
 };
 
@@ -52,12 +50,37 @@ export async function getSetlistFromLiveFans(): Promise<Setlist> {
 function App() {
   const [inputValue, setInputValue] = useState('')
 
+  const [setlist, setSetlist] = useState<Setlist | null>(null); // setlistの状態を追加
+
+  const generate_url = () => {  // URLからID部分を取得　
+    if (inputValue.includes("setlist.fm")) {
+      const last_hyphen_index = inputValue.lastIndexOf("-")
+      const dot_html_index = inputValue.lastIndexOf(".html")
+
+      const id_part = inputValue.substring(last_hyphen_index + 1, dot_html_index);
+
+      return id_part
+    } else if (inputValue.includes("livefans.jp")) {
+      const last_slash_index = inputValue.lastIndexOf("/")
+      const id_part = inputValue.substring(last_slash_index + 1)
+      return id_part
+    }
+  }
+
   const setlistFM = async () => {
     console.log('Input value:', inputValue);
 
+    const id_part = generate_url();
+
+    if (!id_part) { // id_partがundefinedの場合の処理
+      console.error('ID part is undefined');
+      return; // 処理を中断
+    }
+
     try {
-      let setlist = await getSetlistFromSetlistFm('43ab9f07');
-      console.log('Setlist:', setlist.songs);
+      let fetchedSetlist = await getSetlistFromSetlistFm(id_part);
+      console.log('Setlist:', fetchedSetlist);
+      setSetlist(fetchedSetlist); // 取得したsetlistを状態に保存      
     } catch (error) {
       console.error('Error:', error);
     }
@@ -67,9 +90,17 @@ function App() {
   const liveFans = async () => {
     console.log('Input value:', inputValue);
 
+    const id_part = generate_url();
+
+    if (!id_part) { // id_partがundefinedの場合の処理
+      console.error('ID part is undefined');
+      return; // 処理を中断
+    }
+
     try {
-      let setlist = await getSetlistFromLiveFans();
+      let setlist = await getSetlistFromLiveFans(id_part);
       console.log('Setlist:', setlist);
+      setSetlist(setlist); // 取得したsetlistを状態に保存
     } catch (error) {
       console.error('Error:', error);
     }
@@ -83,8 +114,16 @@ function App() {
         <Field value={inputValue} onChange={setInputValue} />
         <br />
         <Button onClick={setlistFM}>setlistFMから作成</Button>
+        <br />
+        <br />
         <Button onClick={liveFans}>LiveFansから作成</Button>
       </div>
+
+      <Iframe
+        url={setlist ? `https://open.spotify.com/embed/playlist/${setlist.setlist_id} ` : ""}
+        width="100%"
+        height="600px"
+      />
     </>
   )
 }
--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Field = ({ value, onChange }: { value: string; onChange: (value: string) => void }) => {
     return (
         <>
@@ -9,7 +7,6 @@ const Field = ({ value, onChange }: { value: string; onChange: (value: string) =
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
                 className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://www.setlist.fm/" required />
-
         </>
     )
 }
