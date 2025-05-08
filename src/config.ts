1f8732929371a42969b30b70bf22a749289660be
TAPIOKA1113 <manato20021113@gmail.com>
2025-05-08 16:00:42 +0900

    本番環境とローカルの出し分け

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ const isDevelopment = import.meta.env.MODE === 'development';
 // APIのベースURL
 export const API_BASE_URL = isDevelopment
   ? 'http://localhost:3030'
-  : 'https://0gri69uq0g.execute-api.ap-northeast-1.amazonaws.com/prod';
+  : 'https://jllyl7rl5d.execute-api.ap-northeast-1.amazonaws.com/Prod/';
 
 // APIエンドポイント
 export const API_ENDPOINTS = {
